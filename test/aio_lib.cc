a3736a35d90d614c84a738a7bea2290bf5605b94
JiYou <jiyou09@gmail.com>
2018-11-04 19:45:04 -0700

    aio: - 1KB 1KB 2KB mutiple read request send success. - Can not send read & write at the same time.
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- test/aio_lib.cc
+++ test/aio_lib.cc
@@ -52,12 +52,22 @@ struct aio_env {
     }
   }
 
-  void Prepare(int fd, uint64_t offset, char *out, uint32_t size) {
+  void PrepareRead(int fd, uint64_t offset, char *out, uint32_t size) {
     // prepare the io
     iocb[index].aio_fildes = fd;
     iocb[index].u.c.offset = offset;
     iocb[index].u.c.buf = out;
     iocb[index].u.c.nbytes = size;
+    iocb[index].aio_lio_opcode = IO_CMD_PREAD;
+    index++;
+  }
+
+  void PrepareWrite(int fd, uint64_t offset, char *out, uint32_t size) {
+    iocb[index].aio_fildes = fd;
+    iocb[index].u.c.offset = offset;
+    iocb[index].u.c.buf = out;
+    iocb[index].u.c.nbytes = size;
+    iocb[index].aio_lio_opcode = IO_CMD_PWRITE;
     index++;
   }
 
@@ -110,7 +120,7 @@ int main(void) {
   }
 
   auto read_test = [&](uint64_t offset, uint64_t size) {
-    ev.Prepare(fd, offset, buf, size);
+    ev.PrepareRead(fd, offset, buf, size);
     ev.Submit();
     ev.WaitOver();
     ev.Clear();
@@ -121,7 +131,56 @@ int main(void) {
   };
 
   read_test(0, polar_race::kPageSize);
+
+  uint64_t sum_char = 0;
+  char backup[4096];
+  for (uint32_t i = 0; i < polar_race::kPageSize; i++) {
+    sum_char += buf[i];
+    backup[i] = buf[i];
+  }
+
   read_test(1024, 1024);
+
+
+  // read 1KB, 1KB, 2KB into buf.
+  auto two_read_request = [&]() {
+    char *a = buf, *b = buf + 1024;
+    char *c = buf + 2048;
+    ev.PrepareRead(fd, 0, a, 1024); // read first 1KB
+    ev.PrepareRead(fd, 1024, b, 1024); // read second 1KB
+    ev.PrepareRead(fd, 2048, c, 2048); // read 3,4 1KB
+    ev.Submit();
+    ev.WaitOver();
+    ev.Clear();
+  };
+
+  memset(buf, 0, polar_race::kPageSize);
+  two_read_request();
+
+  for (uint32_t i = 0; i < polar_race::kPageSize; i++) {
+    sum_char -= buf[i];
+    if (buf[i] != backup[i]) {
+      std::cout << "meet error" << std::endl;
+    }
+  }
+  if (sum_char) {
+    std::cout << "meet error" << std::endl;
+  } else {
+    std::cout << "read success" << std::endl;
+  }
+
+  char *write_content = polar_race::GetAlignedBuffer(polar_race::kPageSize);
+  memcpy(write_content, backup, polar_race::kPageSize);
+  // read 1KB, 1KB, 2KB into buf.
+  auto write_request = [&]() {
+    char *a = buf, *b = buf + 1024;
+    char *c = buf + 2048;
+    ev.PrepareWrite(fd, polar_race::kPageSize, write_content, polar_race::kPageSize);
+    ev.Submit();
+    ev.WaitOver();
+    ev.Clear();
+  };
+  write_request();
 
   close(fd);
   free(buf);
