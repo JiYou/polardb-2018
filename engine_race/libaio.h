806851a621c58dbffda83eeab1439987a8412f04
JiYou <jiyou09@gmail.com>
2018-11-09 22:04:28 -0700

    Delete useless files.
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- engine_race/libaio.h
+++ engine_race/libaio.h
@@ -1,27 +1,4 @@
-/* /usr/include/libaio.h
- *
- * Copyright 2000,2001,2002 Red Hat, Inc.
- *
- * Written by Benjamin LaHaise <bcrl@redhat.com>
- *
- * libaio Linux async I/O interface
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- */
-#ifndef __LIBAIO_H
-#define __LIBAIO_H
+#pragma once
 
 #ifdef __cplusplus
 extern "C" {
@@ -312,5 +289,3 @@ static inline void io_set_eventfd(struct iocb *iocb, int eventfd)
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* __LIBAIO_H */
