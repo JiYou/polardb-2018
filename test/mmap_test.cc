7ad552ac92a1b93824bed1c2b08c2b43e4a54d80
JiYou <jiyou09@gmail.com>
2018-11-10 00:37:29 -0700

    Change back to use smaller file size.
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- test/mmap_test.cc
+++ test/mmap_test.cc
@@ -20,6 +20,18 @@ constexpr uint64_t kBigFileSize{279172874240ull};
 
 #define MAP_HUGE_1GB (30 << MAP_HUGE_SHIFT)
 
+void Read(char *buf, std::string *value) {
+  char *a = buf + 4096;
+
+  std::cout << "char*addr = " << static_cast<const void*>(a) << std::endl;
+  std::string tmp(a, 4096);
+  std::cout << "temp addr = " << static_cast<const void*>(tmp.data()) << std::endl;
+  std::cout << "---swap---" << std::endl;
+  value->swap(tmp);
+  std::cout << "swap value addr = " << static_cast<const void*>(value->data()) << std::endl;
+  std::cout << "temp addr = " << static_cast<const void*>(tmp.data()) << std::endl;
+}
+
 int main() {
   std::string file = std::string(kEnginePath) + "/" + "DB";
   int fd = open(file.c_str(), O_RDWR | O_DIRECT, 0644);
@@ -28,7 +40,7 @@ int main() {
     return 0;
   }
 
-  void* ptr = mmap(NULL, kMaxIndexSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGE_1GB|MAP_NONBLOCK|MAP_POPULATE, fd, 0);
+  void* ptr = mmap(NULL, kBigFileSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGE_1GB|MAP_NONBLOCK|MAP_POPULATE, fd, kMaxIndexSize);
   if (ptr == MAP_FAILED) {
     printf("mmap failed!\n");
     close(fd);
@@ -36,10 +48,18 @@ int main() {
   }
 
   char *buf = (char*)ptr;
-  printf("[] = %c\n", buf[0]);
+  printf("[] = %c\n", buf[4096<<12]);
+
+  std::string value;
+  std::cout << "define addr = " << static_cast<const void*>(value.data()) << std::endl;
+
+  Read(buf, &value);
+
+  std::cout << "called addr = " << static_cast<const void*>(value.data()) << std::endl;
+  //std::cout << value << std::endl;
 
 
-  munmap(ptr, kMaxIndexSize);
+  munmap(ptr, kBigFileSize);
 
   close(fd);
   return 0;
