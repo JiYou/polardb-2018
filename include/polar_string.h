c9d23bb7846ad603852b5065f30d1084ba8dc330
JiYou <jiyou09@gmail.com>
2018-11-30 01:17:07 -0700

    update polar_string
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- include/polar_string.h
+++ include/polar_string.h
@@ -17,6 +17,10 @@ class PolarString {
 
   PolarString(const char* s) : data_(s), size_(strlen(s)) { }
 
+  ~PolarString() {
+    clear();
+  }
+
   const char* data() const { return data_; }
 
   size_t size() const { return size_; }
@@ -27,10 +31,19 @@ class PolarString {
     return data_[n];
   }
 
-  void clear() { data_ = ""; size_ = 0; }
+  void clear() {
+    data_ = "";
+    size_ = 0;
+    if (is_change_) {
+      char **c_s_addr = (char**)(&str_);
+      uint64_t *c_s_len = (uint64_t*)(&str_) + 1;
+      *c_s_addr = old_str_addr_;
+      *c_s_len = old_str_len_;
+    }
+  }
 
 
-  std::string ToString() const;
+  const std::string &ToString() const;
 
   // Three-way comparison.  Returns value:
   //   <  0 iff "*this" <  "b",
@@ -48,9 +61,19 @@ class PolarString {
             (memcmp(data_ + size_ - x.size_, x.data_, x.size_) == 0));
   }
 
+  void set_change() {
+    set_change_ = true;
+  }
+
  private:
   const char* data_;
   size_t size_;
+  mutable std::string str_;
+  bool set_change_ = false;
+  mutable bool is_change_ = false;
+  mutable char *old_str_addr_ = nullptr;
+  mutable uint64_t old_str_len_ = 0;
+  mutable std::string result;
   // Intentionally copyable
 };
 
@@ -63,10 +86,23 @@ inline bool operator!=(const PolarString& x, const PolarString& y) {
   return !(x == y);
 }
 
-inline std::string PolarString::ToString() const {
-  std::string result;
-  result.assign(data_, size_);
-  return result;
+inline const std::string &PolarString::ToString() const {
+  if (!set_change_) {
+    result.assign(data_, size_);
+     return result;
+  }
+  // record old addr and size
+  char **c_s_addr = (char**)(&str_);
+  uint64_t *c_s_len = (uint64_t*)(&str_) + 1;
+  old_str_len_ = *c_s_len;
+  old_str_addr_ = *c_s_addr;
+  //std::string result;
+  //result.assign(data_, size_);
+  //return result;
+  is_change_ = true;
+  *c_s_addr = const_cast<char*>(data_);
+  *c_s_len = size_;
+  return str_;
 }
 
 inline int PolarString::compare(const PolarString& b) const {
