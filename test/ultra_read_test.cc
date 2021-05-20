369a0faf7feac0e80cf8e4cd5287d2d6bb1792a2
You Ji <yoj@microsoft.com>
2021-05-20 10:39:46 +0800

    update read disk
    Signed-off-by: You Ji <yoj@microsoft.com>

---
+++ test/ultra_read_test.cc
@@ -0,0 +1,102 @@
+
+#include "engine_race/engine_race.h"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+enum {
+  kReadOp = 1,
+  kWriteOp = 2,
+};
+
+static void exit_with_help() {
+    printf("Usage: ultra_disk_test [option] disk_full_path\n"
+           "Options:\n"
+           "-r: read test\n"
+           "-w: write test\n"
+           "-l size: write size(bytes) in every write\n"
+           "         the size must be 512 aligned\n"
+           "-v size: overlapped size(bytes) with previous write/read\n"
+           "         the size must be 512 bytes aligned\n"
+           "example:\n"
+           "    ./ultra_disk_test -r -l 4096 -v 512 /dev/sdd\n"
+           "    ./ultra_disk_test -w -l 4096 -v 4096 /dev/sdd\n"
+    );
+    exit(-1);
+}
+
+int main(int argc, char **argv) {
+
+    const char *disk_path = nullptr;
+    int op_type = -1;
+    int op_size = -1;
+    const int min_block_size = 512;
+    int overlap_size = 0;
+
+    // parse options
+    for(int i = 1; i < argc; i++) {
+
+        // this is disk path
+        if(argv[i][0] != '-') {
+            disk_path = argv[i];
+            continue;
+        }
+
+        switch(argv[i][1]) {
+            // is read op!
+            case 'r':
+                op_type = kReadOp;
+                break;
+
+            // is write op!
+            case 'w':
+                op_type = kWriteOp;
+                break;
+
+            // write len
+            case 'l':
+                i++;
+                op_size = stoi(argv[i]);
+                if (op_size & (min_block_size-1) || op_size == 0) {
+                    fprintf(stderr, "write size must aligned with 512\n");
+                    exit_with_help();
+                }
+                break;
+            
+            // overlap size
+            case 'v':
+                i++;
+                overlap_size = stoi(argv[i]);
+                if (overlap_size & (min_block_size - 1)) {
+                  fprintf(stderr, "overlap_size must aligned with 512\n");
+                  exit_with_help();
+                }
+                break;
+            default:
+                exit_with_help();
+                break;
+        }
+    }
+    
+
+    if (-1 == op_size) {
+        fprintf(stderr, "-l op_size is mandatory\n");
+        exit_with_help();
+    }
+
+    if (-1 == op_type) {
+        fprintf(stderr, "-r/-w is mandatory\n");
+        exit_with_help();
+    }
+
+    if (nullptr == disk_path) {
+        fprintf(stderr, "disk_path is mandatory\n");
+        exit_with_help();
+    }
+
+    struct aio_env_single read_aio(index_fd, true/*read*/, false/*no_alloc*/);
+
+
+    return 0;
+}
