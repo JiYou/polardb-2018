af8112164f488ba5a0a2dc858b9417e23d8a7666
root <root@yoj-perf-e4v3.gg0mhj1a4z1ejhvpw02v5lwhgb.bx.internal.cloudapp.net>
2021-05-27 06:42:15 +0000

    update csf format
    Signed-off-by: root <root@yoj-perf-e4v3.gg0mhj1a4z1ejhvpw02v5lwhgb.bx.internal.cloudapp.net>

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -7,17 +7,14 @@
 
 using namespace std;
 
-enum
-{
+enum {
   kReadOp = 1,
   kWriteOp = 2,
   kBlockSize = 512,
 };
 
-static void
-exit_with_help()
-{
-  printf("Usage: ultra_disk_test [option] disk_full_path\n"
+static void exit_with_help() {
+  printf("Usage: test_csv [option] dir_path\n"
          "Options:\n"
          "-r: read test\n"
          "-w: write test\n"
@@ -25,27 +22,64 @@ exit_with_help()
          "         the size must be %d aligned\n"
          "-v size: overlapped size(bytes) with previous write/read\n"
          "         the size must be %d bytes aligned\n"
-         "-t time: run how many seconds\n"
+         "-d disk_Size: filter the tested disk size (GByte)\n"
          "example:\n"
-         "    ./ultra_disk_test -r -l 4096 -v %d /dev/sdd\n"
-         "    ./ultra_disk_test -w -l 4096 -v 4096 /dev/sdd\n",
-         kBlockSize,
-         kBlockSize,
-         kBlockSize);
+         "    ./test_csv -r -l 4096 -v %d ./test-512G\n"
+         "    ./test_csv -w -l 4096 -v 4096 ./test-102G\n",
+         kBlockSize, kBlockSize, kBlockSize);
+
   exit(-1);
 }
 
+// filter arguments.
+// -1 stands for any value.
 string dir_path;
 int op_type = -1;
 int op_size = -1;
-int overlap_size = 0;
+int overlap_size = -1;
+int disk_size = -1;
+double percentile = -1;
 
-void
-printdir(const char* dir)
-{
+struct file_info {
+  int op_type;
+  int op_size;
+  int overlap_size;
+  int disk_size;
+  double percentile;
+};
 
-  DIR* dp;
-  struct dirent* entry;
+static vector<string> to_words(string &s) {
+  const int N = s.length();
+  vector<string> ans;
+
+  int front = -1;
+  for (int i = 0; i <= N; i++) {
+    if (i == N || s[i] == '-') {
+      if (front + 1 < i && s[front + 1] != '-') {
+        // [front+1, ..., i) is a word
+        string word = s.substr(front + 1, i - front - 1);
+        ans.push_back(word);
+      }
+    }
+  }
+
+  return ans;
+}
+
+static struct file_info filter_file(string file_name) {
+  auto words = to_words(s);
+
+  struct file_info ans;
+
+  auto disk_size = words[0];
+  disk_size.pop_back();
+  ans.disk_size = stoi(disk_size.c_str());
+}
+
+static void printdir(const char *dir) {
+
+  DIR *dp;
+  struct dirent *entry;
 
   if ((dp = opendir(dir)) == NULL) {
     printf("open %s error\n", dir);
@@ -53,8 +87,9 @@ printdir(const char* dir)
   }
 
   while ((entry = readdir(dp)) != NULL) {
-
     struct stat statbuf;
+    memset(&statbuf, 0, sizeof(statbuf));
+
     lstat(entry->d_name, &statbuf);
 
     if (S_ISDIR(statbuf.st_mode)) {
@@ -64,12 +99,17 @@ printdir(const char* dir)
       printf("%s\n", entry->d_name);
     }
   }
+
   closedir(dp);
 }
 
-int
-main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
+
+  // {512, ....,, 16384};
+  unordered_set<int> disk_size_set;
+  for (int i = 9; i <= 14; i++) {
+    disk_size_set.insert(1 << i);
+  }
 
   // parse options
   for (int i = 1; i < argc; i++) {
@@ -81,40 +121,54 @@ main(int argc, char** argv)
     }
 
     switch (argv[i][1]) {
-      // is read op!
-      case 'r':
-        op_type = kReadOp;
-        break;
+    // is read op!
+    case 'r':
+      op_type = kReadOp;
+      break;
 
-      // is write op!
-      case 'w':
-        op_type = kWriteOp;
-        break;
+    // is write op!
+    case 'w':
+      op_type = kWriteOp;
+      break;
 
-      // write len
-      case 'l':
-        i++;
-        op_size = stoi(argv[i]);
-        if (op_size & (kBlockSize - 1) || op_size == 0) {
-          fprintf(stderr, "write size must aligned with %d\n", kBlockSize);
-          exit_with_help();
-        }
-        break;
-
-      // overlap size
-      case 'v':
-        i++;
-        overlap_size = stoi(argv[i]);
-        if (overlap_size & (kBlockSize - 1)) {
-          fprintf(stderr, "overlap_size must aligned with %d\n", kBlockSize);
-          exit_with_help();
-        }
-        break;
-
-      default:
+    // write len
+    case 'l':
+      i++;
+      op_size = stoi(argv[i]);
+      if (op_size & (kBlockSize - 1) || op_size == 0) {
+        fprintf(stderr, "write size must aligned with %d\n", kBlockSize);
         exit_with_help();
-        break;
+      }
+      break;
+
+    // overlap size
+    case 'v':
+      i++;
+      overlap_size = stoi(argv[i]);
+      if (overlap_size & (kBlockSize - 1)) {
+        fprintf(stderr, "overlap_size must aligned with %d\n", kBlockSize);
+        exit_with_help();
+      }
+      break;
+
+    // disk size in GB
+    case 'd':
+      i++;
+      disk_size = stoi(argv[i]);
+      if (!disk_size_set.count(disk_size)) {
+        fprintf(stderr, "overlap_size must aligned with %d\n", kBlockSize);
+        exit_with_help();
+      }
+      break;
+
+    default:
+      exit_with_help();
+      break;
     }
+  }
+
+  if (dir_path.length() == 0) {
+    exit_with_help();
   }
 
   printdir(dir_path.c_str());
