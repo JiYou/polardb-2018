8a05130bde9791f57f749f76218af7b1a46e37d8
root <root@yoj-perf-4g.gg0mhj1a4z1ejhvpw02v5lwhgb.bx.internal.cloudapp.net>
2021-06-25 15:03:23 +0000

    update code
    Signed-off-by: root <root@yoj-perf-4g.gg0mhj1a4z1ejhvpw02v5lwhgb.bx.internal.cloudapp.net>

--- test/single_scsi_test.cc
+++ test/single_scsi_test.cc
@@ -2,6 +2,40 @@
 #include "engine_race/engine_race.h"
 #include "engine_race/util.h"
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <scsi/sg.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <scsi/scsi_ioctl.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define __STDC_FORMAT_MACROS 1
+#include <inttypes.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#ifndef major
+#include <sys/types.h>
+#endif
+#include <linux/major.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <byteswap.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <linux/fs.h>
@@ -13,6 +47,11 @@
 #include <unistd.h>
 
 #include <bits/stdc++.h>
+
+#ifndef RAW_MAJOR
+#define RAW_MAJOR 255 /*unlikely value */
+#endif
+
 
 using namespace std;
 using namespace polar_race;
@@ -246,6 +285,265 @@ output_result(int op_type,
   }
 }
 
+namespace SCSI {
+
+/**
+ * Utilities can use these exit status values for syntax errors and
+ * file (device node) problems (e.g. not found or permissions).
+ * command line syntax problem
+ **/
+#define SG_LIB_SYNTAX_ERROR 1
+
+#define SENSE_BUFF_LEN 64 /* Arbitrary, could be larger */
+#define DEF_TIMEOUT 40000 /* 40,000 millisecs == 40 seconds */
+
+#define FT_OTHER 1  /* filetype other than sg and ... */
+#define FT_SG 2     /* filetype is sg char device */
+#define FT_RAW 4    /* filetype is raw char device */
+#define FT_BLOCK 8  /* filetype is block device */
+#define FT_ERROR 64 /* couldn't "stat" file */
+
+constexpr uint32_t KiB = 1024;
+constexpr uint32_t MiB = KiB * KiB;
+constexpr uint32_t k4KiB = KiB << 2;
+
+inline void Sleep(int ms) {
+    if (ms <= 0) {
+      return;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+static std::pair<int, std::string>
+file_type_list[] = {
+  {FT_OTHER, "FT_OTHER"},
+  {FT_SG, "FT_SG"},
+  {FT_RAW, "FT_RAW"},
+  {FT_BLOCK, "FT_BLOCK"},
+  {FT_ERROR, "FT_ERROR"},
+};
+
+// 去拿文件的类型
+static int get_file_type(const char *filename) {
+  struct stat st;
+
+  if (stat(filename, &st) < 0) {
+    return FT_ERROR;
+  }
+
+  if (S_ISCHR(st.st_mode)) {
+    if (RAW_MAJOR == major(st.st_rdev)) {
+      return FT_RAW;
+    } else if (SCSI_GENERIC_MAJOR == major(st.st_rdev)) {
+      return FT_SG;
+    }
+  } else if (S_ISBLK(st.st_mode)) {
+    return FT_BLOCK;
+  }
+  return FT_OTHER;
+}
+
+// 输出文件的类型
+static void print_file_type(int flag) {
+  const int N = sizeof(file_type_list) / sizeof(*file_type_list);
+  for (int i = 0; i < N; i++) {
+    const int mask = file_type_list[i].first;
+    if (flag & mask) {
+      printf("%s | ", file_type_list[i].second.c_str());
+    }
+  }
+  printf("\n");
+}
+
+
+// 文件操作
+static const char *file_name = "/dev/sg2";
+int fd = -1;
+
+
+// 写入之后是否要检查
+static bool do_verify = false;
+
+
+#define VERIFY10 0x2f
+#define VERIFY12 0xaf
+#define VERIFY16 0x8f
+
+
+static inline void sg_put_unaligned_be16(uint16_t val, void *p) {
+        uint16_t u = bswap_16(val);
+        memcpy(p, &u, 2);
+}
+
+static inline void sg_put_unaligned_be32(uint32_t val, void *p) {
+        uint32_t u = bswap_32(val);
+        memcpy(p, &u, 4);
+}
+
+// 开始构建SCSI命令!
+static int
+sg_build_scsi_cdb(uint8_t *cdbp,
+                  int cdb_sz,
+                  unsigned int blocks,
+                  int64_t start_block,
+                  bool is_verify,
+                  bool write_true,
+                  bool fua,
+                  bool dpo)
+{
+    int sz_ind;
+    int rd_opcode[] = {0x8, 0x28, 0xa8, 0x88};
+    int ve_opcode[] = {0xff /* no VERIFY(6) */,
+                       VERIFY10,
+                       VERIFY12,
+                       VERIFY16};
+    int wr_opcode[] = {0xa, 0x2a, 0xaa, 0x8a};
+
+    memset(cdbp, 0, cdb_sz);
+
+    // 先把检查打开
+    assert(is_verify == true);
+    // 我们先考虑为10的时候的处理
+    assert(cdb_sz == 10);
+
+    cdbp[1] = 0x2;
+
+    is_verify = false;
+        if (dpo)
+      cdbp[1] |= 0x10;
+    if (fua)
+      cdbp[1] |= 0x8;
+
+    sz_ind = 1;
+    if (is_verify && write_true) {
+      cdbp[0] = ve_opcode[sz_ind];
+    } else {
+      cdbp[0] = (uint8_t)(write_true ? wr_opcode[sz_ind] :
+        rd_opcode[sz_ind]);
+    }
+
+    sg_put_unaligned_be32(start_block, cdbp + 2);
+    sg_put_unaligned_be16(blocks, cdbp + 7);
+
+    if (blocks & (~0xffff)) {
+      printf("for 10 byte commands, maximum number of blocks is "
+                "%d\n", 0xffff);
+      return SG_LIB_SYNTAX_ERROR;
+    }
+
+    return 0;
+}
+
+/**
+ * coe:
+ * 0: exit on error (def)
+ * 1: continue on sg error (zero fill)
+ * 2: also try read_long on unrecovered reads,
+ * 3: and set the CORRCT bit on the read long
+ **/
+static int coe = 0;
+
+/* failed but coe set */
+constexpr int G_DD_BYPASS = 999;
+
+uint8_t wrCmd[1024];
+uint8_t senseBuff[1024];
+int pack_id_count;
+
+/**
+ *
+ * 函数功能：
+ *
+ *  调用SCSI命令写一个设备!
+ *  Does a SCSI WRITE or VERIFY (if do_verify set) on OFILE.
+ *
+ * 输入参数：
+ *
+ *    infd:       文件句柄
+ *    wrkPos:     内存位置
+ *    blocks:     要读的blocks的数目
+ *    skip:       从哪里开始读?
+ *    bs:         block的大小
+ *    scsi_cdbsz: cmd缓冲区的大小
+ *    fua:        fua要用吗？
+ *    dpo:        device cache要不要被调度出去!
+ *    &diop:      diop值有可能被改变
+ *
+ * 返回值：
+ *
+ * 0:                           successful
+ * SG_LIB_SYNTAX_ERROR:         unable to build cdb,
+ * SG_LIB_CAT_NOT_READY:
+ * SG_LIB_CAT_UNIT_ATTENTION:
+ * SG_LIB_CAT_MEDIUM_HARD:
+ * SG_LIB_CAT_ABORTED_COMMAND:
+ * -2:                          recoverable (ENOMEM),
+ * -1:                          unrecoverable error + others
+ * SG_DD_BYPASS:                failed but coe set.
+ *
+ **/
+static int
+sg_write(int sg_fd,
+         uint8_t *buff,
+         int blocks,
+         int64_t to_block,
+         int bs,
+         int cdbsz,
+         bool fua,
+         bool dpo,
+         bool *diop)
+{
+    bool info_valid;
+    int res;
+    uint64_t io_addr = 0;
+    struct sg_io_hdr io_hdr;
+
+    assert(0 == sg_build_scsi_cdb(wrCmd,
+                          cdbsz,
+                          blocks,
+                          to_block,
+                          true/*verify*/,
+                          true/*write_true*/,
+                          fua,
+                          dpo));
+    memset(&io_hdr, 0, sizeof(struct sg_io_hdr));
+
+    io_hdr.interface_id = 'S';
+    io_hdr.cmd_len = cdbsz;
+    io_hdr.cmdp = wrCmd;
+    io_hdr.dxfer_direction = SG_DXFER_TO_DEV;
+    io_hdr.dxfer_len = bs * blocks;
+    io_hdr.dxferp = buff;
+    io_hdr.mx_sb_len = SENSE_BUFF_LEN;
+    io_hdr.sbp = senseBuff;
+    io_hdr.timeout = DEF_TIMEOUT;
+    io_hdr.pack_id = pack_id_count++;
+
+    if (diop && *diop) {
+        io_hdr.flags |= SG_FLAG_DIRECT_IO;
+    }
+
+    while (((res = ioctl(sg_fd, SG_IO, &io_hdr)) < 0) &&
+           ((EINTR == errno) || (EAGAIN == errno) || (EBUSY == errno))) {
+        /*Nothing*/;
+    }
+
+    assert(res == 0);
+
+//    if (diop && *diop &&
+//        ((io_hdr.info & SG_INFO_DIRECT_IO_MASK)
+//          != SG_INFO_DIRECT_IO)) {
+//      printf("write NOT over!\n");
+//      *diop = false;
+//    }
+
+    return 0;
+}
+
+
+} // end namespace SCSI
+
 int
 main(int argc, char** argv)
 {
@@ -349,7 +647,7 @@ main(int argc, char** argv)
   struct aio_env_single aio(fd, is_read, false /*no_alloc*/);
 
   // setup buffer
-  char* buf = polar_race::GetAlignedBuffer(op_size + overlap_size);
+  uint8_t* buf = (uint8_t*)polar_race::GetAlignedBuffer(op_size + overlap_size);
   for (int i = 0; i < op_size + overlap_size; i++) {
     buf[i] = i % 26 + 'a';
   }
@@ -374,14 +672,26 @@ main(int argc, char** argv)
   for (int i = 0; i < total_write_test; i++) {
     auto start = std::chrono::system_clock::now();
 
-    aio.Prepare(write_pos - overlap_size, buf, op_size + overlap_size);
-    aio.Submit();
+    //aio.Prepare(write_pos - overlap_size, buf, op_size + overlap_size);
+    // aio.Submit();
+    // auto ret = aio.WaitOver();
+    bool direct_io = true;
+    auto ret = SCSI::sg_write(fd,
+                              buf,
+                             (op_size + overlap_size) / kBlockSize    /*block per write*/,
+                              (write_pos - overlap_size) / kBlockSize /*write_to_block_pos*/,
+                              kBlockSize/*block_size*/,
+                              10/*SCSI cmd length*/,
+                              false/*FUA*/,
+                              false/*dpo*/,
+                              &direct_io);
+
 
     write_pos += op_size;
     if (write_pos >= disk_size) {
       write_pos = begin_pos;
     }
-    auto ret = aio.WaitOver();
+
     auto end = std::chrono::system_clock::now();
 
     uint64_t current_io_nanosecond = get_diff_ns(start, end).count();
